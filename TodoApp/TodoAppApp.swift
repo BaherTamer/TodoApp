a3371678ac20c8bb1ad3107171a5b421d8c14f2e
BaherTamerHashem <baher.appledev@gmail.com>
2022-11-25 17:18:35 +0200

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Baher Tamer on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
